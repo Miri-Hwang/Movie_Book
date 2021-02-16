661c268245e9ea2b3106f30676c3b7575e115200
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-16 15:54:17 +0900

    feat : Create User Model & Display on Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Adin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "bio",
+                    "preferences",
+                    "language",
+                    "favorite_book_genre",
+                    "favorite_movie_genre",
+                )
+            },
+        ),
+    )
+
+    list_filter = (
+        "preferences",
+        "language",
+        "favorite_book_genre",
+        "favorite_movie_genre",
+    )
--- users/models.py
+++ users/models.py
@@ -1,3 +1,54 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    """ Custom User Model """
+
+    PREFERENCES_BOOKS = "books"
+    PREFERENCES_MOVIES = "movies"
+
+    PREFERENCES_CHOICES = (
+        (PREFERENCES_BOOKS, "Books"),
+        (PREFERENCES_MOVIES, "Movies"),
+    )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_ENGLISH, "English"),
+    )
+
+    BGENRE_POEM = "poems"
+    BGENRE_NOVEL = "novels"
+    BGENRE_ESSAYS = "essays"
+
+    BGENRE_CHOICES = (
+        (BGENRE_POEM, "Poems"),
+        (BGENRE_NOVEL, "Novels"),
+        (BGENRE_ESSAYS, "Essays"),
+    )
+
+    MGENRE_HORROR = "horror"
+    MGENRE_ROMANCE = "romance"
+    MGENRE_COMEDY = "comedy"
+
+    MGENRE_CHOICES = (
+        (MGENRE_HORROR, "Horror"),
+        (MGENRE_COMEDY, "Comedy"),
+        (MGENRE_ROMANCE, "Romance"),
+    )
+
+    bio = models.TextField(blank=True)
+    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    preferences = models.CharField(
+        choices=PREFERENCES_CHOICES, max_length=6, blank=True
+    )
+    favorite_book_genre = models.CharField(
+        choices=BGENRE_CHOICES, max_length=6, blank=True
+    )
+    favorite_movie_genre = models.CharField(
+        choices=MGENRE_CHOICES, max_length=7, blank=True
+    )
