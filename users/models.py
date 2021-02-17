c229bfb11fbafd916f78440866ca4dd2fe90e96c
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-17 16:37:15 +0900

    fix : Add On_delete

--- users/models.py
+++ users/models.py
@@ -21,34 +21,14 @@ class User(AbstractUser):
         (LANGUAGE_ENGLISH, "English"),
     )
 
-    BGENRE_POEM = "poems"
-    BGENRE_NOVEL = "novels"
-    BGENRE_ESSAYS = "essays"
-
-    BGENRE_CHOICES = (
-        (BGENRE_POEM, "Poems"),
-        (BGENRE_NOVEL, "Novels"),
-        (BGENRE_ESSAYS, "Essays"),
-    )
-
-    MGENRE_HORROR = "horror"
-    MGENRE_ROMANCE = "romance"
-    MGENRE_COMEDY = "comedy"
-
-    MGENRE_CHOICES = (
-        (MGENRE_HORROR, "Horror"),
-        (MGENRE_COMEDY, "Comedy"),
-        (MGENRE_ROMANCE, "Romance"),
-    )
-
     bio = models.TextField(blank=True)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
     preferences = models.CharField(
         choices=PREFERENCES_CHOICES, max_length=6, blank=True
     )
-    favorite_book_genre = models.CharField(
-        choices=BGENRE_CHOICES, max_length=20, blank=True
+    favorite_book_genre = models.ForeignKey(
+        "categories.Categories", max_length=20, blank=True, on_delete=models.CASCADE
     )
-    favorite_movie_genre = models.CharField(
-        choices=MGENRE_CHOICES, max_length=20, blank=True
+    favorite_movie_genre = models.ForeignKey(
+        "categories.Categories", max_length=20, blank=True, on_delete=models.CASCADE
     )
