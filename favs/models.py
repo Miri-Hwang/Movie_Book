4c9ff504e77a73430c00a66f21e52f52b61bb8f2
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-18 14:02:51 +0900

    feat : Add str & Set Blank True

--- favs/models.py
+++ favs/models.py
@@ -10,8 +10,11 @@ class FavList(core_models.TimeStampedModel):
         "users.User",
         on_delete=models.CASCADE,
     )
-    books = models.ManyToManyField("books.Book")
-    movies = models.ManyToManyField("movies.Movie")
+    books = models.ManyToManyField("books.Book", blank=True)
+    movies = models.ManyToManyField("movies.Movie", blank=True)
 
     class Meta:
         verbose_name_plural = "Fav Lists"
+
+    def __str__(self):
+        return f"{self.created_by}'s favorites"
