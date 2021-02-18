bbb09d621866ac7b3648dfdcec7a35b91afb5f3d
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-18 09:55:20 +0900

    feat : Replace Genre with Categories
    using manytomanyfield

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from categories import models as categories_model
 
 # Create your models here.
 class User(AbstractUser):
@@ -26,9 +27,9 @@ class User(AbstractUser):
     preferences = models.CharField(
         choices=PREFERENCES_CHOICES, max_length=6, blank=True
     )
-    favorite_book_genre = models.ForeignKey(
-        "categories.Categories", max_length=20, blank=True, on_delete=models.CASCADE
+    favorite_book_genre = models.ManyToManyField(
+        categories_model.Categories, related_name="book_article_set"
     )
-    favorite_movie_genre = models.ForeignKey(
-        "categories.Categories", max_length=20, blank=True, on_delete=models.CASCADE
+    favorite_movie_genre = models.ManyToManyField(
+        categories_model.Categories, related_name="movie_article_set"
     )
