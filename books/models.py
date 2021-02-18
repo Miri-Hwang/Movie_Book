b207e442acabf92b75c94191ec684cd0479473e6
Miri-Hwang <72543560+Miri-Hwang@users.noreply.github.com>
2021-02-18 12:53:33 +0900

    feat : Create Books Models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core import models as core_models
+from people import models as people_models
 
-# Create your models here.
+
+class Book(core_models.TimeStampedModel):
+
+    """ Book Model Definition """
+
+    Rating_CHOICES = ((1, 1), (2, 2), (3, 3), (4, 4), (5, 5))
+    title = models.CharField(max_length=40)
+    year = models.IntegerField()
+    category = models.ManyToManyField("categories.Categories")
+    cover_image = models.ImageField()
+    rating = models.IntegerField(choices=Rating_CHOICES)
+    writer = models.ManyToManyField("people.Person")
